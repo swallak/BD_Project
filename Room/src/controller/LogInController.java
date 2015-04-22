0c17dca70350ba945a9e1a429ffb2568407973e2
Saad Ouassil Allak <sw.allak@gmail.com>
2015-04-22 15:44:44 +0200

    Start of LogInController implementation
    -Database is need for tests
    -LogInController not yet fully functionnal
    -Only the create account button still need to be handled in SignInView

--- Room/src/controller/LogInController.java
+++ Room/src/controller/LogInController.java
@@ -2,4 +2,23 @@ package controller;
 
 public class LogInController {
 
+    private String pseudo;
+    private String birthday;
+
+    public LogInController(String pseudo, String birthday) {
+        this.pseudo = pseudo;
+        this.birthday = birthday;
+        connect();
+    }
+
+    /**
+     * Verify database and connect the user to the his room's home page
+     *
+     * @return true if connection succeded.
+     */
+    private boolean connect() {
+
+        System.out.println("Connecting " + pseudo +"...");
+        return true;
+    }
 }
