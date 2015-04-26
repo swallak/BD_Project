a90f4bc48793c72185e9aac73870a74a76e5c2be
Fabien Chiffre <fabien.chiffre@gmail.com>
2015-04-26 11:13:11 +0200

    Implementation de OpponentDAO et OpponentDAO_JDBC
    Un cache est mis en place pour ne pas refaire une requête pour chaque
    adversaire.
    
    On passe quand même par une requête même si l'adversaire n'est
    composé que du pseudo pour le moment. Cela assure plus de modularité
    si on souhaite récupérer d'avantage d'information sur une adversaire.

--- Room/src/dao/OpponentDAO.java
+++ Room/src/dao/OpponentDAO.java
@@ -3,10 +3,15 @@ package dao;
 import java.sql.Connection;
 
 import model.user.Opponent;
+import model.user.User;
 
 public abstract class OpponentDAO {
 
-	public abstract Opponent findAvailableOpponent(Connection con, boolean withCommit);
+	public abstract Opponent findOpponent(Connection con, boolean withCommit,
+			String pseudo) throws OpponentNotExistException;
+
+	public abstract Opponent findAvailableOpponent(Connection con,
+			boolean withCommit, User currentUser) throws OpponentNotAvailableException;
 
 	public class OpponentNotExistException extends Exception {
 		private static final long serialVersionUID = 6305537615090513026L;
@@ -14,6 +19,14 @@ public abstract class OpponentDAO {
 		public OpponentNotExistException(String pseudo) {
 			super("Erreur impossible : l'adversaire " + pseudo
 					+ " n'existe pas.");
+		}
+	}
+
+	public class OpponentNotAvailableException extends Exception {
+		private static final long serialVersionUID = 6645484244850608984L;
+
+		public OpponentNotAvailableException() {
+			super("Pas d'adversaire disponible.");
 		}
 	}
 }
--- Room/src/dao/jdbc/DataBaseConstant.java
+++ Room/src/dao/jdbc/DataBaseConstant.java
@@ -2,12 +2,47 @@ package dao.jdbc;
 
 public class DataBaseConstant {
 
-	/* Declaration des noms de colone necessaires. */
-	protected final static String USER_TABLE_NAME = "";
-	protected final static String USER_PSEUDO = "";
-	protected final static String USER_FIRSTNAME = "";
-	protected final static String USER_LASTNAME = "";
-	protected final static String USER_MAIL = "";
-	protected final static String USER_BIRTHDAY = "";
-	
+	/* Table Joueur */
+	protected final static String PLAYER_TABLE_NAME = "Joueur";
+	protected final static String PLAYER_PSEUDO = "pseudo";
+	protected final static String PLAYER_FIRSTNAME = "prenom";
+	protected final static String PLAYER_LASTNAME = "nom";
+	protected final static String PLAYER_MAIL = "mail";
+	protected final static String PLAYER_BIRTHDAY = "dateNaissance";
+	protected final static String PLAYER_ADDRESS_NUMBER = "adrNumero";
+	protected final static String PLAYER_ADDRESS_STREET = "adrRue";
+	protected final static String PLAYER_ADDRESS_POSTAL_CODE = "adrCodePostal";
+	protected final static String PLAYER_ADDRESS_CITY = "adrVille";
+
+	/* Table JoueursParPartie */
+	protected final static String PLAYER_ON_MATCH_TABLE_NAME = "JoueursParPartie";
+	protected final static String PLAYER_ON_MATCH_MATCH_ID = "idPartie";
+	protected final static String PLAYER_ON_MATCH_PLAYER_PSEUDO = "pseudo";
+
+	/* Table Partie */
+	protected final static String MATCH_TABLE_NAME = "Partie";
+	protected final static String MATCH_START_DATE = "dateDemarage";
+	protected final static String MATCH_ID = "idPartie";
+
+	/* Table Action */
+	protected final static String ACTION_TABLE_NAME = "Action";
+	protected final static String ACTION_MATCH_ID = "idPartie";
+	protected final static String ACTION_BOAT_ID = "idBateau";
+	protected final static String ACTION_TURN = "tour";
+	protected final static String ACTION_NUM_ACTION = "numAction";
+
+	/* Table ActionDeplacement */
+	protected final static String ACTION_MOVE_TABLE_NAME = "ActionDeplacement";
+	protected final static String ACTION_MOVE_MATCH_ID = "idPartie";
+	protected final static String ACTION_MOVE_TURN = "turn";
+	protected final static String ACTION_MOVE_NUM_ACTION = "numAction";
+	protected final static String ACTION_MOVE_TYPE = "typeDeplacement";
+
+	/* Table ActionTir */
+	protected final static String ACTION_SHOT_TABLE_NAME = "ActionTir";
+	protected final static String ACTION_SHOT_MATCH_ID = "idPartie";
+	protected final static String ACTION_SHOT_TURN = "tour";
+	protected final static String ACTION_SHOT_NUM_ACTION = "numAction";
+	protected final static String ACTION_SHOT_TARGET_X = "cibleTirX";
+	protected final static String ACTION_SHOT_TARGET_Y = "cibleTirY";
 }
