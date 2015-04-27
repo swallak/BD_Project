86a23babaec9e0f34ab13d92da73a48f6c5eb7fd
Saad Ouassil Allak <sw.allak@gmail.com>
2015-04-27 16:10:04 +0200

    Nothing

--- Room/src/view/Room.java
+++ Room/src/view/Room.java
@@ -35,6 +35,7 @@ public class Room {
                 
                 //SignIn Frame location and size
                 //SignInFrame.setLocation(0, 150);
+                
                 SignInFrame.setSize(500, 500);
                 SignInFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 SignInFrame.setVisible(true);
